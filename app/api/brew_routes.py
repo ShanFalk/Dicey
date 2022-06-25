4b047d77783e15e31e7011ccabc0251baf3427b6
Josh Keim <joshuamkeim@gmail.com>
2022-06-25 11:41:05 -0400

    added aws helper functions

---
+++ app/api/brew_routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, jsonify, session, request
+from app.models import Brew, db
+from app.forms.brew_form import CreateBrew
+from app.aws_utils import get_unique_filename, upload_file_to_s3
+from flask_login import current_user, login_user, logout_user, login_required
+
+
+brew_routes = Blueprint('brews', __name__)
+
+
+@brew_routes.route("/")
+@login_required
+def add_brew():
+    form = CreateBrew()
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    file = request.files[form.image.name]
+    if form.validate_on_submit():
+        if file.filename == "":
+            return "Please select a file"
+        if file:
+            file.filename = get_unique_filename(file.filename)
+            upload_file_to_s3(file)
