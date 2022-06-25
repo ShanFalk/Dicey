c44ec8f689cd561e0cd292621b4a78545680b5ee
Shannon Falk <falk.shannon@gmail.com>
2022-06-25 16:17:35 -0400

    finished reviews seed file

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -6,28 +6,75 @@ def seed_reviews():
         [
             Review(
         rating=5,
-        content='This brew took my gaming experience to a whole new level!',
+        content='Lorem ipsum dolor sit amet, consectetur adipiscing elit',
         userId=1,
         brewId=1
         ),
             Review(
         rating=4,
-        content='Very unique content! Had a lot of fun with it.',
+        content='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         userId=2,
         brewId=1
         ),
             Review(
         rating=1,
-        content='Booooooriiiiiing',
+        content='Ut enim ad minim veniam',
         userId=3,
         brewId=2
         ),
             Review(
         rating=3,
-        content='',
+        content='quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         userId=1,
+        brewId=2
+        ),
+            Review(
+        rating=2,
+        content='Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        userId=2,
+        brewId=3
+        ),
+        Review(
+        rating=5,
+        content='Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+        userId=3,
+        brewId=3
+        ),
+            Review(
+        rating=4,
+        content='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        userId=2,
+        brewId=4
+        ),
+            Review(
+        rating=1,
+        content='Ut enim ad minim veniam',
+        userId=3,
+        brewId=4
+        ),
+            Review(
+        rating=3,
+        content='quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        userId=3,
         brewId=1
+        ),
+            Review(
+        rating=2,
+        content='Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        userId=1,
+        brewId=4
+        ),
+            Review(
+        rating=3,
+        content='quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        userId=1,
+        brewId=5
         ),
 
         ]
     )
+    db.session.commit()
+
+def undo_reviews():
+    db.session.execute('TRUNCATE reviews RESTART IDENTITY CASCADE;')
+    db.session.commit()
