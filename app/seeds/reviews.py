7ea8fa62fd900c14469b7c9a02553ef086697e17
Chase Riddick <chaseriddick@outlook.com>
2022-06-26 10:27:38 -0700

    Corrected

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -1,44 +1,4 @@
 from app.models import db, Review
-<<<<<<< HEAD
-
-
-def seed_reviews():
-    review_seed_1 = Review(rating = 3,
-                content = 'Seen better',
-                user_id = 1,
-                brew_id = 2)
-
-    review_seed_2 = Review(rating = 5,
-                content = 'Awesome.',
-                user_id = 2,
-                brew_id = 1)
-
-    review_seed_3 = Review(rating = 2,
-                content = 'Meh.',
-                user_id = 3,
-                brew_id = 2)
-
-    review_seed_3 = Review(rating = 2,
-                content = 'Meh.',
-                user_id = 3,
-                brew_id = 2)
-
-    review_seed_4 = Review(rating = 2,
-                content = "Would buy it again if it wasn't a pdf.",
-                user_id = 3,
-                brew_id = 1)
-
-    db.session.add(review_seed_1)
-    db.session.add(review_seed_2)
-    db.session.add(review_seed_3)
-    db.session.add(review_seed_4)
-
-    db.session.commit()
-
-def undo_reviews():
-    db.session.execute('TRUNCATE purchases (RESTART IDENTITY CASCADE;')
-    db.session.commit()
-=======
 #check the name of the model!
 
 def seed_reviews():
@@ -118,4 +78,3 @@ def seed_reviews():
 def undo_reviews():
     db.session.execute('TRUNCATE reviews RESTART IDENTITY CASCADE;')
     db.session.commit()
->>>>>>> c80a3071e74a20228af700650f0a65e45cdce5e3
