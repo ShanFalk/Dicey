f2fa684483f0c7d3ee4681363576adaeca2091d9
Chase Riddick <chaseriddick@outlook.com>
2022-06-27 09:11:30 -0700

    Refactored in Shannons changess

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,7 +4,8 @@ import { useDispatch } from 'react-redux';
 
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/NavBar';
+import NavBar from './components/NavigationComponents/Navbar';
+import Search from './components/NavigationComponents/Search';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
@@ -64,6 +65,7 @@ function App() {
         </Route>
 
         <Route path='/search?q=:userInput' exact={true} >
+          <Search />
           <SearchResultDisplay />
         </Route>
 
