65b977e7bc82e005c3fede9b74d0aefad5a7f764
Josh Keim <joshuamkeim@gmail.com>
2022-06-27 19:35:29 -0400

    fixed import statements

--- react-app/src/components/BrewsCollection/MainPageDisplay/FeaturedBrewsCollection/index.js
+++ react-app/src/components/BrewsCollection/MainPageDisplay/FeaturedBrewsCollection/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 
 function FeaturedBrewsCollection ({brew}) {
-    const image = brew.images[0]
+    //const image = brew?.images[0]
 
     return (
         <div className='card-block'>
         <h3>This is the BrewsColComponents - Main Page Display - FeaturedBrewsCollection</h3>
-        <img className="brew-image" src={image?.img_url}
-        alt="" /> 
+        {/* <img className="brew-image" src={image?.img_url}
+        alt="" />  */}
           <div className='card-info'>
               <h2>{brew.title}</h2>
               <p>$ {brew.price}</p>
