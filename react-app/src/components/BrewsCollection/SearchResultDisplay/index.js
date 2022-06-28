7b21403f4a28f2685a529b923552e9ca829f8c7e
connorwfitch <connorwfitch@gmail.com>
2022-06-28 22:16:35 +0000

    done with search

--- react-app/src/components/BrewsCollection/SearchResultDisplay/index.js
+++ react-app/src/components/BrewsCollection/SearchResultDisplay/index.js
@@ -4,14 +4,26 @@ import { useLocation } from "react-router-dom";
 
 function SearchResultDisplay () {
     const brews = useSelector(state => state.brews);
-    let { search } = useLocation();
-    search = search.slice(6).toLowerCase()
+
+    let search = useLocation().search;
+    let tagId = new URLSearchParams(search).get('tagId');
+    tagId = parseInt(tagId, 10);
+    search = search.split('&tagId=')[0].slice(6).toLowerCase().split(' ');
 
     const results = Object.values(brews).filter((brew) => {
-        //TODO check if search in tags (loop)
-        return brew.title.toLowerCase().includes(search) || brew.description.toLowerCase().includes(search)
+        if (tagId) {
+            for (let tag of brew.brew_tags) {
+                if (tag.id === tagId) return true;
+            }
+        }
+
+        for (let term of search) {
+            if (brew.title.toLowerCase().includes(term) || brew.description.toLowerCase().includes(term)) {
+                return true;
+            }
+        }
     })
-    console.log('THESE ARE THE RESULTS', results)
+
     return (
         <>
         <h2>Results</h2>
