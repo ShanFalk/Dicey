1bfc3788efefed3250b53e25816f3e9be028c818
Shannon Falk <falk.shannon@gmail.com>
2022-06-29 17:07:28 -0400

    fixed total bug and delete bug

---
+++ react-app/src/components/ShoppingCart/RemoveItem/index.js
@@ -0,0 +1,15 @@
+function RemoveItem({ brewIds, brewId, deleteEnd}) {
+
+    function remove(e) {
+        e.preventDefault();
+
+        const updatedCart = brewIds.filter((ele) => ele !== brewId);
+        localStorage.setItem('cart', JSON.stringify(updatedCart));
+        deleteEnd();
+    }
+    return (
+        <button onClick={remove}>Remove</button>
+    )
+}
+
+export default RemoveItem;
