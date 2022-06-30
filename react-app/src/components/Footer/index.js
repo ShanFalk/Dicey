d1e76171043b76c93fbd2cf56e5a2ebcb2754f3b
connorwfitch <connorwfitch@gmail.com>
2022-06-30 17:22:05 +0000

    basic footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,9 +1,19 @@
+import { Link } from 'react-router-dom';
+import "./footer.css"
+
 function Footer() {
     // TODO: github, link to about page, link to etsy, names(link to our individual github)
     return (
-        <>
-        
-        </>
+        <div className='footer'>
+            <Link to='/about' className='footerlink'>
+                About
+            </Link>
+            <a href='https://www.etsy.com' className='footerlink'>Etsy</a>
+            <a href='https://github.com/ShanFalk' className='footerlink'>Shannon Falk</a>
+            <a href='https://github.com/Chase-Riddick' className='footerlink'>Chase Riddick</a>
+            <a href='https://github.com/connorwfitch' className='footerlink'>Connor Fitch</a>
+            <a href='https://github.com/keimjm' className='footerlink'>Josh Keim</a>
+        </div>
     )
 }
 
