aa872b09be1b8a182a2aa7a9f12e5e2cdb1bf95d
Chase Riddick <chaseriddick@outlook.com>
2022-07-01 14:34:50 -0700

    stashing changes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -17,6 +17,7 @@ import BrewPage from './components/BrewSingle/BrewPage';
 import BrewCreateForm from './components/BrewSingle/BrewCreateForm/index';
 import NotFound from './components/NotFound';
 import Footer from './components/Footer';
+import SVGBottom from './components/Footer/SVGBottom';
 import { authenticate } from './store/session';
 import { getAllBrews } from './store/brew';
 import { getAllTags } from './store/tag';
@@ -112,19 +113,7 @@ function App() {
 
       </Switch>
 
-      <div className="custom-shape-divider-bottom-1656629992">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M1200,0H0V120H281.94C572.9,116.24,602.45,3.86,602.45,3.86h0S632,116.24,923,120h277Z" className="shape-fill"></path>
-        </svg>
-      </div>
-
-      <div className="custom-shape-divider-bottom-1656635902">
-      <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
-      </svg>
-      </div>
-
-
+      <SVGBottom />
       </div>
       <Footer />
     </BrowserRouter>
