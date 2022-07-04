d19e1da0b1b93b72bf939fbf19a92c4c305c5939
Shannon Falk <falk.shannon@gmail.com>
2022-07-04 12:26:11 -0400

    second possible fix for timestamp

--- react-app/src/utils/relative-time.js
+++ react-app/src/utils/relative-time.js
@@ -25,5 +25,5 @@ export default function convertToTimeAgo(dateObj) {
     dateString += dateObjSlices[4]
 
 
-    return moment.utc(dateString, "YYYYMMDDhh:mm:ss").fromNow()
+    return moment(moment(dateString, "YYYYMMDDhh:mm:ss").local()).fromNow()
 };
