c4a4151ee8d2e76ce20559728d12e6f8d81b0dc8
Shannon Falk <falk.shannon@gmail.com>
2022-08-05 07:30:45 -0400

    fixed shopping cart bug and revised review seeds

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -7,112 +7,120 @@ def seed_reviews():
         [
             Review(
                 rating=5,
-                content='Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                content='AMAZING CONTENT',
                 user_id=1,
                 brew_id=4
             ),
             Review(
                 rating=4,
-                content='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                content='Pretty darn good!',
                 user_id=1,
                 brew_id=8
             ),
             Review(
                 rating=1,
-                content='Ut enim ad minim veniam',
+                content='Waste of money.',
                 user_id=1,
                 brew_id=1
             ),
             Review(
                 rating=3,
-                content='quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                content='It was okay. Could have used more of the ol\' razzle dazzle.',
                 user_id=1,
                 brew_id=13
             ),
             Review(
                 rating=2,
-                content='Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                content='Needs some serious improvement. Kind of boring content.',
                 user_id=2,
                 brew_id=1
             ),
             Review(
                 rating=5,
-                content='Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                content='Really great value! You certainly get your money\'s worth!',
                 user_id=2,
                 brew_id=12),
             Review(
                 rating=4,
-                content='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                content='My friends and I really liked this!.',
                 user_id=2,
                 brew_id=4
             ),
             Review(
                 rating=1,
-                content='Ut enim ad minim veniam',
+                content='OMG no, just don\'t bother.',
                 user_id=1,
                 brew_id=4
             ),
             Review(
                 rating=3,
-                content='quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                content='I would say this one is pretty good!',
                 user_id=3,
                 brew_id=6
             ),
             Review(
                 rating=2,
-                content='Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+                content='Yeesh. Not the worst, but certainly not the best.',
                 user_id=3,
                 brew_id=14
             ),
             Review(
                 rating=3,
-                content='Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque.',
+                content='I thought I was going to be wowed, but it\'s just okay.',
                 user_id=3,
                 brew_id=15
             ),
             Review(
                 rating=4,
-                content='Vulputate sapien nec sagittis aliquam malesuada. Imperdiet massa tincidunt nunc pulvinar sapien.',
+                content='Really added something special to our campaign.',
                 user_id=3,
                 brew_id=5
             ),
             Review(
                 rating=2,
-                content='Elementum tempus egestas sed sed risus pretium quam vulputate. Montes nascetur ridiculus mus mauris.',
+                content='If they can improve their content, I\'ll revise my review.',
                 user_id=2,
                 brew_id=1
-            ), Review(
+            ),
+            Review(
                 rating=4,
-                content='Vulputate sapien nec sagittis aliquam malesuada. Imperdiet massa tincidunt nunc pulvinar sapien.',
+                content='I really enjoyed this.',
                 user_id=3,
                 brew_id=2
-            ), Review(
+            ),
+            Review(
                 rating=4,
-                content='Augue neque gravida in fermentum et sollicitudin ac orci.',
+                content='The artwork is incredible!',
                 user_id=2,
                 brew_id=3
-            ), Review(
+            ),
+            Review(
                 rating=5,
-                content='Vulputate sapien nec sagittis aliquam malesuada. Imperdiet massa tincidunt nunc pulvinar sapien.',
+                content='A++ Outstanding.',
                 user_id=1,
-                brew_id=7), Review(
-                rating=4,
-                content='Vulputate sapien nec sagittis aliquam malesuada. Imperdiet massa tincidunt nunc pulvinar sapien.',
-                user_id=3,
-                brew_id=9),
+                brew_id=7
+            ),
+            Review(
+            rating=4,
+            content='This one is my favorite so far.',
+            user_id=3,
+            brew_id=9
+            ),
             Review(
                 rating=5,
-                content='Vulputate sapien nec sagittis aliquam malesuada. Imperdiet massa tincidunt nunc pulvinar sapien.',
+                content='Highly recommend!',
                 user_id=3,
-                brew_id=10),
+                brew_id=10
+            ),
             Review(
                 rating=4,
-                content='Vulputate sapien nec sagittis aliquam malesuada. Imperdiet massa tincidunt nunc pulvinar sapien.',
+                content='I hope they post more; this was great!',
                 user_id=3,
-                brew_id=11),
+                brew_id=11
+            ),
             Review(
                 rating=3,
-                content='Felis imperdiet proin fermentum leo vel. Arcu ac tortor dignissim convallis aenean et. Eu augue ut lectus arcu.',
+                content='Not as good as what I was expecting, but overall nice.',
                 user_id=3,
                 brew_id=2),
 
